1226078bad30301438ed7e15205d40393dadb93a
prandp <32594507+prandp@users.noreply.github.com>
2018-04-14 23:33:44 +0530

    new portfolio site
    a complete new portfolio design

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ $(function(){
 	new WOW().init();
 });
 
+   var typed = new Typed(".type", {
+  strings: [
+  "I am",
+  "a developer",
+  "a designer",
+  "and an artist"
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop:true,
+  cursorChar: "|",
+});
+
