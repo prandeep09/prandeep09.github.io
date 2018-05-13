57acb7372918f2f3048fd8d484fb3690672978d4
prandp <32594507+prandp@users.noreply.github.com>
2018-05-13 23:32:08 +0530

    change of plan
    a lot changes than the last time. Theme color, style ... yes a lot

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
+
+
 $(function(){
 	new WOW().init();
 });
+/*$('.main-iso').isotope({
+	itemSelector:'.item',
+	layoutMode:'fitRows'
+});
+
+$('iso-nav ul li').click(function(){
+	$('.iso-nav ul li').removeClass('active');
+	$(this).addClass('active');
+
+	var selector = $(this).attr('data-filter');
+	$('main-iso').isotope({
+		filter: selector;
+	});
+	return flase;
+});
+$('.main-iso').isotope({
+	itemSelector:'.item',
+	layoutMode:'fitRows'
+});
+
+$('iso-nav ul li').click(function(){
+	$('.iso-nav ul li').removeClass('active');
+	$(this).addClass('active');
+
+	var selector = $(this).attr('data-filter');
+	$('main-iso').isotope({
+		filter: selector;
+	});
+	return flase;
+});
+
+
+
+
 
    var typed = new Typed(".type", {
   strings: [
@@ -20,7 +56,9 @@ document.getElementById("knowmorebtn").onclick = function(){
 }; 
 document.getElementById("p2-viewsite").onclick = function(){
 	location.href = "images/underconstruction.jpg";
-};     
+}; */    
 //--------------------------------------------- porfolio.js -------------------------------------//
+//--------------------------------------------- isotop.js -------------------------------------//
+
 
 
